f1877b2dbcb9fef7b05dcd4450673917fc8729cc
Rubens <rubencarmonareyes50@gmail.com>
2026-02-04 14:22:05 -0600

    Arreglos 1

---
+++ testarreglos.java
@@ -0,0 +1,13 @@
+public class testarreglos
+{
+    int arreglo[], arreglo4;   //arreglo 4 es entero
+    int []arreglo2, arreglo5;  //ambos son arreglos
+    int[] arreglo3, arreglo6;  //ambos son arreglos
+
+    public testarreglos()
+    {
+        arreglo = new int[5];
+        arreglo2 = arreglo;
+        arreglo[2]=50;
+    }
+}
